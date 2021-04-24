57847965c6f31482d7415f29b9d94135d92ea047
cgustavop <carlosgsp39@gmail.com>
2021-04-24 03:57:56 +0100

    implementação de novas funções

--- src/parser.c
+++ src/parser.c
@@ -82,8 +82,14 @@ void parse(char *line) {
         else if (strcmp(token, "_") == 0)	// função duplica
         	duplica(s);
 
-        else if (strcmp(token, "\\n") == 0)   // função troca 
+        else if (strcmp(token, "\\") == 0)   // função troca 
             troca(s);
+
+        else if (strcmp(token, "@") == 0)   // função troca 3 
+            rodar(s);
+
+        else if (strcmp(token, "$") == 0)   // função troca topo por n-esimo
+            copia(s);
 
         // pops e prints
         
@@ -93,10 +99,10 @@ void parse(char *line) {
         else if (strcmp(token, "p") == 0)   // printa top
             printt(s);
 
-        /*
+        
         else if (strcmp(token, "l") == 0)   // lê linha abaixo
             lerl(s);
-
+/*
         else if (strcmp(token, "t") == 0)   // lê todas linhas
             lert(s); 
         */
